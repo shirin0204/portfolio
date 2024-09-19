38656c5477a21ac9be13485c8458468bfe7398e3
shirin0204 <shirin0204@gmail.com>
2024-09-19 13:21:17 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+// import Amplify from 'aws-amplify';
+// import aws_exports from './aws-exports';
+// import { AmplifyProvider } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
 
-createApp(App).mount('#app')
+// Amplify.configure(aws_exports);
+
+const app = createApp(App);
+
+// app.component(AmplifyProvider.name, AmplifyProvider);
+
+app.mount('#app');
